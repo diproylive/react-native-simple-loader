d10e9ff6e06c85899962e05a144a69f15e1930e9
Dip Roy <119170245+diproylive@users.noreply.github.com>
2025-09-14 21:13:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
-import React, { useState } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
-import Loader from './src/Loader'; // ✅ Import your Loader
+import React, { useState } from "react";
+import { View, Button } from "react-native";
+import Loader from "react-native-simple-loader";
 
 const App = () => {
   const [loading, setLoading] = useState(false);
 
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Button title="Show Loader" onPress={() => setLoading(true)} />
-      <Loader visible={loading} overlay color="red" boxWidth={100} boxHeight={100}/>
-      <Loader visible={loading} overlay color="red" />
-      <Loader visible={loading} overlay boxWidth={100} boxHeight={100} type="dot" size= 'small'/>
-      <Loader visible={loading} overlay type="pulse" color="blue"  boxWidth={100} boxHeight={100}/>
-      <Loader visible={loading} overlay type="wave" color="green" boxWidth={100} boxHeight={100}/>
-      <Loader visible={loading} overlay type="bar"  boxWidth={100} boxHeight={100}/>
+      <Loader visible={loading}  boxWidth={100} boxHeight={100}  />
     </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
